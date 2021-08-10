83c44d11192b5c543f7e14e3091c402d1aaf7078
David Bunk <bunk@biologie.uni-muenchen.de>
2021-08-10 09:48:59 +0200

    added cpu gpu check

--- bentoserver.py
+++ bentoserver.py
@@ -9,11 +9,26 @@ torch.jit.script = script
 import os
 os.environ['PATH'] += (os.path.dirname(os.path.realpath(__file__)))
 
+import argparse
 from bentoml.server import start_dev_server
 from multiprocessing import freeze_support
 
 path = os.getcwd()
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('port', type=int, help='Port of bentoml server.')
+    parser.add_argument('device', type=str, help='Set device to cpu or gpu.')
+    args = parser.parse_args()
+
     freeze_support()
-    start_dev_server(os.path.join(path, 'yeastmate'), 5000, False, False)
+
+    if args.device == 'cpu':
+        module = 'yeastmate_cpu'
+    elif args.device == 'gpu' and torch.cuda.is_available() == True:
+        module = 'yeastmate_gpu'
+        print('true')
+    else:
+        module = 'yeastmate_cpu'
+
+    start_dev_server(os.path.join(path, module), args.port, False, False)
